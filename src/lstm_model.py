5564d6a49e4131aed1ea2119db7c45626b641bd8
= <=>
2026-01-25 12:24:31 +0300

    adding arima and lstm models

---
+++ src/lstm_model.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+
+def build_lstm_model(input_shape):
+    model = Sequential()
+
+    model.add(LSTM(50, return_sequences=True, input_shape=input_shape))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(50))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(1))
+
+    model.compile(
+        optimizer=Adam(learning_rate=0.001),
+        loss="mse"
+    )
+
+    return model
